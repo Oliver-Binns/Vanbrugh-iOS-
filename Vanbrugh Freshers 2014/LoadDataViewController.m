3aa739ea6337910c28bc5474e21066135b4db832
Oliver Binns <mail@oliverbinns.info>
2014-10-26 12:33:21 +0000

    -Notification Bug Fix

--- Vanbrugh Freshers 2014/LoadDataViewController.m
+++ Vanbrugh Freshers 2014/LoadDataViewController.m
@@ -69,7 +69,11 @@
         localNotification.fireDate = [NSDate dateWithTimeIntervalSince1970:[[notification objectAtIndex:1] doubleValue]];
         
         localNotification.alertBody = [notification objectAtIndex:0];
-        [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+        
+        NSDate *now = [[NSDate alloc] initWithTimeIntervalSinceNow:0];
+        if ([now compare:localNotification.fireDate] == NSOrderedAscending) { //the local notification time is in the future
+            [[UIApplication sharedApplication] scheduleLocalNotification:localNotification]; //only schedules the notification if it is in the future
+        }
     }
 }
 
