6cd38133bd7955a6088149ce2f602369bb449b10
Oliver Binns <mail@oliverbinns.info>
2014-10-27 12:20:16 +0000

    -Fixed bug causing crash on iOS 7.x

--- Vanbrugh Freshers 2014/LoadDataViewController.m
+++ Vanbrugh Freshers 2014/LoadDataViewController.m
@@ -16,6 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSObject *object = [defaults objectForKey:@"block"];
+    if([[NSString stringWithFormat:@"%@", object] isEqualToString:@"STYC"]){
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSString *block = @"Second/Third Year";
+        [defaults setObject:[NSString stringWithString:block] forKey:@"block"];
+        [defaults synchronize];
+    }
     //[self setUpNotifications];
     dispatch_group_t group = dispatch_group_create();
     
@@ -121,11 +129,9 @@
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    if([segue.identifier isEqualToString:@"goToHome"]){
-    }
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
-}
+//}
 
 @end
